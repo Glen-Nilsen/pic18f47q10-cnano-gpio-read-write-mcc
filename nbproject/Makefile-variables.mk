05056077965b2d12c662500f574e223dad8239b5
glen.nilsen@microchip.com <glen.nilsen@microchip.com>
2021-02-26 11:33:05 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=pic18f47q10-cnano-gpio-read-write-mcc.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pic18f47q10-cnano-gpio-read-write-mcc.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pic18f47q10-cnano-gpio-read-write-mcc.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pic18f47q10-cnano-gpio-read-write-mcc.x.tar
+# pro configuration
+CND_ARTIFACT_DIR_pro=dist/pro/production
+CND_ARTIFACT_NAME_pro=pic18f47q10-cnano-gpio-read-write-mcc.X.production.hex
+CND_ARTIFACT_PATH_pro=dist/pro/production/pic18f47q10-cnano-gpio-read-write-mcc.X.production.hex
+CND_PACKAGE_DIR_pro=${CND_DISTDIR}/pro/package
+CND_PACKAGE_NAME_pro=pic18f47q10-cnano-gpio-read-write-mcc.x.tar
+CND_PACKAGE_PATH_pro=${CND_DISTDIR}/pro/package/pic18f47q10-cnano-gpio-read-write-mcc.x.tar
